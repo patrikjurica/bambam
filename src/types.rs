da3a064532484d52d4576179713afd0604bf8f5a
Patrik Juřica <patrik@local>
2026-04-28 20:20:16 +0200

    new: add types, change dependencies

---
+++ src/types.rs
@@ -0,0 +1,17 @@
+use rustc_hash::FxHashMap;
+
+pub type KmerVal = u64;
+
+/// Represents a single rare k-mer anchored to the reference genome.
+///
+/// derive `PartialOrd` and `Ord` for sorting
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct RareKmer {
+    pub start: usize,
+    pub end: usize,
+    pub val: KmerVal,
+    pub local_tolerance: usize,
+}
+
+/// map chromosome names to a sorted list of their rare k-mers.
+pub type KmerLibrary = FxHashMap<String, Vec<RareKmer>>;
