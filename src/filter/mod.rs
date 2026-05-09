c34e3e3f6d851369ea9bebf1651d3960aa1cd942
patrik <patrik.lev.jurica@gmail.com>
2026-05-10 00:41:30 +0200

    new: add sort warning

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 use noodles::bam;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
@@ -13,7 +13,8 @@ mod utils;
 
 /// Filters a BAM file based on absolute positional compliance of rare k-mers.
 /// Acts as a router: if `primary_only` is true, it uses a high-speed sequential stream.
-/// If false, it groups alignments by QNAME (Requires `samtools sort -n`).
+/// If false, it groups alignments by QNAME (Requires `samtools sort -n`)
+/// and parallelizes the processing
 pub fn filter_bam(
     bam_in_path: &str,
     bam_out_path: &str,
@@ -28,6 +29,26 @@ pub fn filter_bam(
         File::open(bam_in_path).context("Failed to open input BAM")?,
     ));
     let header = reader.read_header().context("Failed to read BAM header")?;
+
+    if !primary_only {
+        let is_query_name_sorted = header
+            .header()
+            .map(|hd| format!("{:?}", hd).to_lowercase().contains("queryname"))
+            .unwrap_or(false);
+
+        if !is_query_name_sorted {
+            bail!(
+                "\n[ERROR] BAM file is not sorted by Query Name!\n\n\
+                Due to computational reasons, processing secondary and supplementary alignments requires \
+                the reads to be physically grouped together in the file.\n\n\
+                HOW TO FIX THIS:\n\
+                1. If you want to evaluate secondary alignments: Sort your BAM file by name first \
+                using `samtools sort -n input.bam -o name_sorted.bam`.\n\
+                2. If you only care about primary alignments: Bypass this check entirely by running \
+                this tool with the `--primary-only` flag.\n"
+            );
+        }
+    }
 
     let mut writer = bam::io::Writer::new(BufWriter::with_capacity(
         128 * 1024,
